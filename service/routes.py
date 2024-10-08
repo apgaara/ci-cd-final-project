2e6e73871fb251a6e3f1fcc12b5de1f0ccd61601
apgaara <ap8081485@gmail.com>
2024-10-08 00:34:36 -0400

    fix: characters in service

--- service/routes.py
+++ service/routes.py
@@ -22,7 +22,7 @@ def health():
 ############################################################
 @app.route("/")
 def index():
-    """Returns information abut the service"""
+    """Returns information about the service"""
     app.logger.info("Request for Base URL")
     return jsonify(
         status=status.HTTP_200_OK,
@@ -39,7 +39,9 @@ def index():
 def list_counters():
     """Lists all counters"""
     app.logger.info("Request to list all counters...")
+
     counters = [dict(name=count[0], counter=count[1]) for count in COUNTER.items()]
+
     return jsonify(counters)
 
 
@@ -52,11 +54,10 @@ def create_counters(name):
     app.logger.info("Request to Create counter: %s...", name)
 
     if name in COUNTER:
-        return abort(
-            status.HTTP_409_CONFLICT, f"Counter {name} already exists"
-        )
+        return abort(status.HTTP_409_CONFLICT, f"Counter {name} already exists")
 
     COUNTER[name] = 0
+
     location_url = url_for("read_counters", name=name, _external=True)
     return (
         jsonify(name=name, counter=0),
@@ -91,6 +92,32 @@ def update_counters(name):
     if name not in COUNTER:
         return abort(status.HTTP_404_NOT_FOUND, f"Counter {name} does not exist")
 
-    COUNTER
-    
+    COUNTER[name] += 1
+
+    counter = COUNTER[name]
+    return jsonify(name=name, counter=counter)
+
+
+############################################################
+# Delete counters
+############################################################
+@app.route("/counters/<name>", methods=["DELETE"])
+def delete_counters(name):
+    """Deletes a counter"""
+    app.logger.info("Request to Delete counter: %s...", name)
+
+    if name in COUNTER:
+        COUNTER.pop(name)
+
+    return "", status.HTTP_204_NO_CONTENT
+
+
+# Utility for testing
+def reset_counters():
+    """Removes all counters while testing"""
+    global COUNTER  
+    if app.testing:
+        COUNTER = {}
+
+ 
 __all__ = ['reset_counters', 'app']
